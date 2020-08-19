5bbddaffd19045944164bae349ebd018780cf752
Brian <beavelar.github@gmail.com>
2020-08-18 21:06:09 -0600

    Creation of Shuffle Discord bot

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # Shuffle
+This repository contains code for a Discord bot that displays a random song daily or upon request.
+
+The goal of the bot is to provide a random song at the ease of the user.
+
+## Quick Links:
+- [Project Requirements](#project-requirements)
+- [Bot Permission Requirements](#bot-permission-requirements)
+
+----------------------------------
+
+## Project Requirements
+- Python
+  - [Python Organization Website](https://www.python.org/)
+- Discord library
+  - [Discord.py Installation Guide](https://discordpy.readthedocs.io/en/latest/intro.html)
+
+## Bot Permission Requirements
+The following are permission requirements needed by the MoCk BoT
+ - **General Permissions**
+   - View Channels
+ - **Text Permissions**
+   - Send Messages
+   - Manage Messages
+   - Embed Links
+   - Attach Files
+   - Read Message History
+   - Mention everyone
+   - Use External Emojis
---
+++ shuffle.py
@@ -0,0 +1,50 @@
+import discord
+from discord_util.discord_imp import *
+
+#########################################################################################################
+# Global definitions
+
+MOCK_TRIGGER = '!song'
+
+try: 
+    tokenFile = open('environment/.env', 'r')
+except Exception as ex:
+    print('Exception caught attempting to open environment files')
+    print('Please verify .env file exist and is in the correct location')
+    print('Exiting..')
+    print(str(ex))
+    exit(1)
+
+try:
+    BOT_TOKEN = tokenFile.read()
+except Exception as ex:
+    print('Exception caught attempting to read environment files')
+    print('Exiting..')
+    print(str(ex))
+    exit(1)
+
+client = discord.Client()
+
+#########################################################################################################
+# On_message handler - Discord function that executes after message is detected from Discord server
+#
+# Parameters
+# message: Message (Discord API)
+
+@client.event
+async def on_message(message):
+    # Message isn't from Shuffle bot and message includes !song trigger
+    if (client.user != message.author) and (MOCK_TRIGGER in message.content):
+        await sendMessage(client.user, message.channel, "Bot notified")
+
+#########################################################################################################
+# On_ready handler - Executes after bot starts up
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected')
+
+#########################################################################################################
+# Startup command to start the bot
+
+client.run(BOT_TOKEN)
